43e5592d708d62e33ac5014e74194bc1d79ecad9
LORDBABUINO <jorge.x7@gmail.com>
2021-01-16 00:44:50 -0300

    feat(login): creates login page

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+}
